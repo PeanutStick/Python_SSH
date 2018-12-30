064da59348696fa835bd50655c712061865c030e
PeanutStick <38986405+PeanutStick@users.noreply.github.com>
2018-12-30 22:36:04 +0100

    Add files via upload

---
+++ ssh.py
@@ -0,0 +1,20 @@
+import paramiko
+ssh = paramiko.SSHClient()
+ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+ssh.connect('ip', port=8645, username='user',password='pass')
+ssh.exec_command('ls')
+stdin, stdout, stderr = ssh.exec_command("ls -l")
+for line in stdout.read().splitlines():
+     print (line)
+
+stdin, stdout, stderr = ssh.exec_command ('cd Desktop; pwd; ls -l')
+for line in stdout.read().splitlines():
+     print (line)
+
+#stdin, stdout, stderr = ssh.exec_command("who")
+#for line in stdout.read().splitlines():
+#    print (line)
+
+#stdin, stdout, stderr = ssh.exec_command("ls -l")
+#for line in stdout.read().splitlines():
+#     print (line)
