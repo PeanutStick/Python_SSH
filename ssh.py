4cd7d78ae3fd345d996ea5013b4ad584224b3239
PeanutStick <38986405+PeanutStick@users.noreply.github.com>
2018-12-30 22:46:25 +0100

    Update ssh.py

--- ssh.py
+++ ssh.py
@@ -1,7 +1,7 @@
 import paramiko
 ssh = paramiko.SSHClient()
 ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-ssh.connect('ip', port=8645, username='user',password='pass')
+ssh.connect('ip', port=22, username='user',password='pass')
 ssh.exec_command('ls')
 stdin, stdout, stderr = ssh.exec_command("ls -l")
 for line in stdout.read().splitlines():
